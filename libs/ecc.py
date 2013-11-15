2807fcdcc986e561670d04d7499e3e795b653da7
Peter Sulyok <sulyokpeti@gmail.com>
2013-11-16 00:03:40 +0100

    avoid 'id' key error in rec['user']

--- libs/ecc.py
+++ libs/ecc.py
@@ -146,6 +146,7 @@ def load_records():
     user_headers = {}
     user_records = {}
     for rec in rl:
+      if 'id' in rec['user'].keys():
         uid = rec['user']['id']
         if not uid in urlt.keys():
             urlt[uid] = []
