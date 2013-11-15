b869ff23a42a93c52349c41c0deb8ea288c5d606
Peter Sulyok <sulyokpeti@gmail.com>
2013-11-16 00:53:47 +0100

    try

--- libs/ecc.py
+++ libs/ecc.py
@@ -141,7 +141,8 @@ def load_records():
     for i in rl[100:]:
         if 100 - rl[1] < max_rid: os.unlink
     list_response = [ gzip.compress(json.dumps(rl).encode()) ]
-    print(str(rl).encode())
+    print(json.dumps(rl).encode())
+    print(list_response)
     list_header = json_header.copy()
     list_header[1] = ('Content-Length', str(len(list_response[0])))
     os.chdir(cwd)
